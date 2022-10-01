21586c00e29d01131f1a7a3140e76967b2c5b2d3
Hristo Yonkov <hristopturs@gmail.com>
2022-10-01 13:43:13 +0300

    add routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
-
+const hbs = require('express-handlebars');
+const routes = require('./routes');
 const app = express();
 
-app.use('/static', express.static('public')); // Middleware wich will look for static files!!!...
+app.use('/', express.static('./src/public')); // Middleware wich will look for static files!!!...
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.engine('hbs', hbs.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+app.use(routes);
 
 app.listen(3000, () => console.log('Server is listening on port 3000'));
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const homeController = require('./controllers/home');
+
+router.get('/', homeController.index);
+
+module.exports = router;
+
