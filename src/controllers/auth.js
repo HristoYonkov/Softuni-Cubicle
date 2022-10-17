d810fc06ec4cb65699835c7286275a0d5114ac0e
Hristo Yonkov <hristopturs@gmail.com>
2022-10-17 16:19:16 +0300

    create jwt token on login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -17,9 +17,17 @@ router.post('/register', async (req, res) => {
     }
 });
 
-
 router.get('/login', (req, res) => {
     res.render('auth/login')
+});
+
+router.post('/login', async (req, res) => {
+
+    let token = await authService.login(req.body);
+    if (!token) {
+        return res.redirect('404');
+    }
+    res.redirect('/');
 });
 
 
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,8 @@
 const User = require('../models/User');
-
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+
+const secret = 'asdohjaksjdhxmijhfsciufxmgasildailujhmlcxihjb';
 const saltRounds = 10;
 
 exports.register = async ({username, password, repeatPassword}) => {
@@ -11,10 +13,45 @@ exports.register = async ({username, password, repeatPassword}) => {
 
     let hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    let createdUser = await User.create({
+    let createdUser = User.create({
         username,
         password: hashedPassword
     });
 
+    // let createdUser = new User({             // Second way to create user in database, helps for more dynamic settings with new user properties
+    //     username,
+    //     password: hashedPassword
+    // });
+
+    // createdUser.save();
+
     return createdUser;
+}
+
+exports.login = async ({username, password}) => {
+    let user = await User.findOne({username});
+
+    if (!user) {
+        //TODO : add a message
+        return;
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        return;
+    }
+
+    let result = new Promise((resolve, reject) => {
+        jwt.sign({_id: user._id, username: user.username}, secret, {expiresIn: '2d'}, (err, token) => {
+            if (err) {
+                reject(err);
+            }
+            console.log(token);
+            resolve(token);
+        });
+    })
+
+    return result
+
 }
